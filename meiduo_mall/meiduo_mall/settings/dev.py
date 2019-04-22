059419ca5c56f08599627e7a2f75f4cd7246f565
1923488289 <1923488289@qq.com>
2019-04-22 23:42:47 +0800

    meiduo_all环境的配置

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -14,6 +14,8 @@ import os
 import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
+print(__file__)
 sys.path.insert(0,os.path.join(BASE_DIR,'apps'))
 
 # Quick-start development settings - unsuitable for production
